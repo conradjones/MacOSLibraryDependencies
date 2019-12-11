00d10db4113417bc72618220f187d7a2f5f578fa
Conrad Jones <cjones@turboit.co.uk>
2019-12-11 01:49:17 +0000

    print usage

--- src/main_bundle.cpp
+++ src/main_bundle.cpp
@@ -4,7 +4,8 @@
 int main(int argc, char* argv[])
 {
     if (argc < 3) {
-        std::terminate();
+        std::cout << "Usage:" << argv[0] << " <binary> <library_path1;library_path2>\n";
+        return 1;
     }
     std::string binary = argv[1];
     std::string library_paths_str = argv[2];
